67462fa677e409c3306a777cb1f4bc96b358ea5c
woobensky <soletechim36@gmail.com>
2026-07-28 13:31:17 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/data', methods=['POST'])
+def receive():
+    data = request.json
+    print("📍 Location received:", data)
+    return jsonify({"status": "success"})
+
+@app.route('/')
+def home():
+    return "Server is running 🚀"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
